5ac4057f95fd41d74bad44b7a13bd638dafacc05
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2023-01-04 11:02:53 +0000

    wefw

--- scripts/generate-py.sh
+++ scripts/generate-py.sh
@@ -27,62 +27,8 @@ generate_sdk() {
 }
 npx @openapitools/openapi-generator-cli version-manager set 6.1.0
 
-OPENAPI_FILENAME=".openapi/kas-fleet-manager.yaml"
-PACKAGE_NAME="rhoas_kafka_mgmt_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/kafka_mgmt_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME $PACKAGE_VERSION
-
-OPENAPI_FILENAME=".openapi/srs-fleet-manager.json"
-PACKAGE_NAME="rhoas_service_registry_mgmt_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/registry_mgmt_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME $PACKAGE_VERSION
-
-OPENAPI_FILENAME=".openapi/connector_mgmt.yaml"
-PACKAGE_NAME="rhoas_connector_mgmt_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/connector_mgmt_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
-
-OPENAPI_FILENAME=".openapi/kafka-admin-rest.yaml"
-PACKAGE_NAME="rhoas_kafka_instance_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/kafka_instance_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
-
-echo "generating registry instance SDK "
-
-cd .openapi
-echo "Removing codegen "
-cat registry-instance.json | jq 'del(.paths."x-codegen-contextRoot")' > registry-instance-tmp.json
-mv -f registry-instance-tmp.json registry-instance.json
-
-echo "Ensuring only single tag is created "
-cat registry-instance.json | jq 'walk( if type == "object" and has("tags") 
-       then .tags |= select(.[0])
-       else . end )' > registry-instance-tmp.json
-mv -f registry-instance-tmp.json registry-instance.json
-
-echo "removing invalid datetime definitions"
-sed -i '' 's/date-time/utc-date/' registry-instance.json
-
-cd ..
-
-OPENAPI_FILENAME=".openapi/registry-instance.json"
-PACKAGE_NAME="rhoas_registry_instance_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/registry_instance_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
-
 OPENAPI_FILENAME=".openapi/smartevents_mgmt.yaml"
 PACKAGE_NAME="rhoas_smart_events_mgmt_sdk"
 OUTPUT_PATH="app-services-sdk-python/sdks/smart_events_mgmt_sdk"
-
-generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
-
-OPENAPI_FILENAME=".openapi/service-accounts.yaml"
-PACKAGE_NAME="rhoas_service_accounts_mgmt_sdk"
-OUTPUT_PATH="app-services-sdk-python/sdks/service_accounts_mgmt_sdk"
 
 generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
